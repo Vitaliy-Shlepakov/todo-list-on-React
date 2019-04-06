c954a2637a330774e87e42a5002bced62b080eb3
vitaliy_shlepakov <shlepakov58@gmail.com>
2019-04-06 11:09:17 +0300

    start to styling

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './todo-list-item.css';
 
 const TodoListItem = ({label, important = false}) => {
     const style = {
         color: important ? 'red' : 'black'
     };
         return (
-        <span style={style}>{label}</span>
+        <span style={style}
+              className = "todo-list-item-label">
+            {label}
+        </span>
+
+        // <button type="button" className="btn btn-outline-success btn-sm">Done</button>
     )
 };
 export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import TodoList from './components/todo-list';
 import SearchPanel from'./components/search-panel';
 
 const todoData = [
-    {label: 'Drink cofee', important: false},
-    {label: 'Drink tea', important: true},
-    {label: 'Drink compote', important: false}
+    {label: 'Drink cofee', important: false, id: 0},
+    {label: 'Drink tea', important: true, id: 1},
+    {label: 'Drink compote', important: false, id: 2}
 ]
 
 const App = () => {
     return(
-        <div>
-            <AppHeader/>
+        <div className="container">
+            <AppHeader done={1} toDo={1}/>
             <SearchPanel/>
             <TodoList todos = {todoData}/>
         </div>
