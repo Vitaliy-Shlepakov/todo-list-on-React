7ed2312358a5875d887de26bb4cbfa9c9b05eeb6
vitaliy_shlepakov <shlepakov58@gmail.com>
2019-04-06 10:05:10 +0300

    start

---
+++ src/components/todo-list-item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoListItem = ({label, important = false}) => {
+    const style = {
+        color: important ? 'red' : 'black'
+    };
+        return (
+        <span style={style}>{label}</span>
+    )
+};
+export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from'./components/search-panel';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const todoData = [
+    {label: 'Drink cofee', important: false},
+    {label: 'Drink tea', important: true},
+    {label: 'Drink compote', important: false}
+]
+
+const App = () => {
+    return(
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList todos = {todoData}/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
